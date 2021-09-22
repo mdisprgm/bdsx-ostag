45199aa6f62c99fefae49e6b291b88c288ab0187
mdisprgm <49188795+mdisprgm@users.noreply.github.com>
2021-09-22 23:15:56 +0900

    now prints log after server open

--- index.ts
+++ index.ts
@@ -5,7 +5,8 @@ import { readFileSync } from "fs";
 const value = readFileSync("../plugins/ostag/setting.json").toString();
 const p = JSON.parse(value);
 events.serverOpen.on(() => {
-  if (p["enabled"]) import("./scoretag");
+    if (p["enabled"]) {
+        import("./scoretag");
+        console.log(white("[OSTAG] loaded successfullly"));
+    } else console.log(white("[OSTAG]"), red("not enabled"));
 });
-if (p["enabled"]) console.log(white("[OSTAG] loaded successfullly"));
-else console.log(white("[OSTAG]"), red("not enabled"));
