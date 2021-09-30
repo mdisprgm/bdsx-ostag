3e544c163dd4d51a546a7b60f38908234effbae2
mdisprgm <49188795+mdisprgm@users.noreply.github.com>
2021-09-30 19:14:07 +0900

    fix bugs.

--- index.ts
+++ index.ts
@@ -1,10 +1,31 @@
 import { events } from "bdsx/event";
 import { red, white } from "colors";
-import { readFileSync } from "fs";
 
-const value = readFileSync("../plugins/ostag/setting.json").toString();
-const p = JSON.parse(value);
+import fs = require("fs");
+
+const makeFolder = (dir: string) => {
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+        console.log("[OSTAG] directory", dir, "is generated");
+    } else {
+        console.log("[OSTAG] directory", dir, "exists already");
+    }
+};
+
 events.serverOpen.on(() => {
+    const dbPath = "../ostag/setting.json";
+
+    makeFolder("../ostag");
+
+    console.log(fs.existsSync("../ostag"));
+    const files = fs.readdirSync("../ostag");
+    console.log(files);
+    if (!files.includes("setting.json")) {
+        fs.closeSync(fs.openSync(dbPath, "w"));
+        fs.writeFileSync(dbPath, JSON.stringify({ enabled: true }));
+    }
+    const value = fs.readFileSync("../ostag/setting.json").toString();
+    const p = JSON.parse(value);
     if (p["enabled"]) {
         import("./scoretag");
         console.log(white("[OSTAG] loaded successfullly"));
